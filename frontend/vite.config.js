3d55e46078fc4228de8ab032bafa18ba47440b8e
Ananthak Kumar Vijayan(UST <307457@ust.com>
2026-06-09 12:16:51 +0530

    modified the frontend vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,5 +2,12 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  server: {
+    allowedHosts: [
+      "blacktickets-dev-alb-821224321.us-east-1.elb.amazonaws.com",
+      "localhost",
+      "0.0.0.0"
+    ]
+  }
 });
